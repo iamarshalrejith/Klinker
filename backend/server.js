b35cf5b4519643fb7bb4a9c7f56fcddd6ebf5f0d
Arshal Rejith <iamarshalrejith@gmail.com>
2025-07-30 23:23:51 +0530

    Initial mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
 dotenv.config();
 
 const app = express();
@@ -11,5 +12,6 @@ const PORT = process.env.PORT || 5001;
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
+   connectToMongoDB()
   console.log(`Server started at ${PORT}`);
 });
