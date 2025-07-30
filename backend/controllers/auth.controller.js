e0550779773d7be665bcad7b5bdd43a35a6e14fc
Arshal Rejith <iamarshalrejith@gmail.com>
2025-07-30 22:41:57 +0530

    Auth route setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req,res)=>{
+    res.send("signup Route")
+}
+
+export const login = (req,res)=>{
+    res.send("login Route")
+}
+
+export const logout = (req,res)=>{
+    res.send("logout Route")
+}
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import authRoutes from "./routes/auth.routes.js"
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+
+app.use("/api/auth",authRoutes)
+
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
 });
