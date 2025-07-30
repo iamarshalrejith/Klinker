c7301a1cfc1294a08bd0bdd732432c84ff75d671
Arshal Rejith <iamarshalrejith@gmail.com>
2025-07-30 22:18:05 +0530

    Initial Project Setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
