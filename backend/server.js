cc4921ae147be1527feb7fa820ceed401e7363a2
Arshal Rejith <iamarshalrejith@gmail.com>
2025-08-02 17:58:18 +0530

    Added Message routes and Message controller & protectroute

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
+import protectRoute from "./middleware/protectRoute.js";
 dotenv.config();
 
 const app = express();
@@ -9,7 +12,11 @@ const PORT = process.env.PORT || 5001;
 
 //middleware
 app.use(express.json());
+app.use(cookieParser());
+
+//Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", protectRoute, messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
