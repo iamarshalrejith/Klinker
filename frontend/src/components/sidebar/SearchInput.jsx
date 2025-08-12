3ddfbb7ef01d283b8cea483c0c03f6fde3c7e7b1
Arshal Rejith <iamarshalrejith@gmail.com>
2025-08-12 11:32:55 +0530

    Added Search functionality

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,19 +1,42 @@
-import { FiSearch } from 'react-icons/fi';
+import { useState } from "react";
+import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle search logic
-  };
+	const [search, setSearch] = useState("");
+	const { setSelectedConversation } = useConversation();
+	const { conversations } = useGetConversations();
 
-  return (
-    <form onSubmit={handleSubmit} className="flex items-center gap-2">
-      <input type="text" placeholder="Search..." className="input input-bordered rounded-full" />
-      <button type="submit" className="btn btn-circle bg-gray-500 text-white" aria-label="Search">
-        <FiSearch />
-      </button>
-    </form>
-  );
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!search) return;
+		if (search.length < 3) {
+			return toast.error("Search term must be at least 3 characters long");
+		}
+
+		const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+
+		if (conversation) {
+			setSelectedConversation(conversation);
+			setSearch("");
+		} else toast.error("No such user found!");
+	};
+	return (
+		<form onSubmit={handleSubmit} className='flex items-center gap-2'>
+			<input
+				type='text'
+				placeholder='Search…'
+				className='input input-bordered rounded-full'
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<button type='submit' className='btn rounded-2xl bg-gray-400 text-white'>
+				<IoSearchSharp className='w-6 h-6 outline-none' />
+			</button>
+		</form>
+	);
 };
-
 export default SearchInput;
+
